a9f99252c28aabbc5cab7c00fefd268be55141ad
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-21 22:51:24 -0800

    added query to home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
+//Import the Apollo hook - which comes from ApolloProvider
+import { useQuery } from '@apollo/client';
+
+//Import the querie saved in the utils/queries.js file
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
+  //use useQuery hook to make request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  // If data exists store it in thoughts, otherwise save an empty array.
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
