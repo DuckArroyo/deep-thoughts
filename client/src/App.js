c7de760d5189dd4cc9955f2bfa591ae44dba9b72
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-21 18:32:28 -0800

    safe point before class

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+
+// establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// use the ApolloClient() constructor to instantiate the Apollo Client instance and create the connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
