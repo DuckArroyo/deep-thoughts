a4bc5d2909e0bf04d1171ad5ef2a2cdf9b03a4f1
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-22 17:45:12 -0800

    added signup funct. testing

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+//Expects 2 variables. Returns token and user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//Expects 2 variables. Returns token and user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
